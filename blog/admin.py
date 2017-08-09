2d716296e57683447c8c4c7995dfce780ee4851a
Madhav <jhmadhav@gmail.com>
2017-08-09 01:30:31 -0400

    Sessions in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Post
 from .models import Channel
+from django.contrib.sessions.models import Session
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+    list_display = ['session_key', '_session_data', 'expire_date']
+
+admin.site.register(Session, SessionAdmin)
 
 admin.site.register(Post)
 admin.site.register(Channel)
