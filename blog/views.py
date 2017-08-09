dfb01920ecc1ba607f62271b342abe7923c06cbc
Madhav <jhmadhav@gmail.com>
2017-08-08 20:07:41 -0400

    Login page

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,13 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from .models import Channel
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+@login_required
 def livetv(request):
     posts = Channel.objects.all().order_by('name')
     return render(request, 'blog/livetv.html', {'posts': posts})
